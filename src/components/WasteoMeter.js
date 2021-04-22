806584f5e0345cdd8a28c7a87864f7c8564f60cf
adanmc01 <dev.adanmc01@gmail.com>
2021-04-22 13:16:00 -0500

    addGoogleTags scripts

--- src/components/WasteoMeter.js
+++ src/components/WasteoMeter.js
@@ -3,7 +3,8 @@ import Button from "react-bootstrap/cjs/Button";
 import Col from 'react-bootstrap/cjs/Col'
 import List from './List'
 import Container from "react-bootstrap/cjs/Container";
-const WasteoMeter = ({showSpecifics, setShowSpecifics, allPrograms, totalAmount, total,setTotal}) => {
+
+const WasteoMeter = ({showSpecifics, setShowSpecifics, allPrograms, totalAmount, total, setTotal}) => {
     const click = e => {
         e.preventDefault()
         if (showSpecifics === true) {
@@ -14,22 +15,27 @@ const WasteoMeter = ({showSpecifics, setShowSpecifics, allPrograms, totalAmount,
     let div = formatter.format(totalAmount)
     setTotal(div)
     return (
-        <Container style={{padding:'15px 0', boxShadow:'-15px 15px lightslategray', maxWidth:'600px', margin:'5px', textAlign: 'left',backgroundColor: 'white'}} sm={5} xl={12} >
-               <Col     >
-                   <h2>Waste-o-Meter</h2>
-                       <h1 className={'big'}>
-                           ${
-                                total
-                            }
-                       </h1>
-                       <Button variant={'warning'} onClick={click}>
-                           ShowDetail
-                       </Button>
-                       <List
-                           allPrograms={allPrograms}
-                           setShowSpecifics={setShowSpecifics}
-                           showSpecifics={showSpecifics}/>
-               </Col>
+        <Container style={{
+            padding: '15px 0',
+            boxShadow: '-15px 15px lightslategray',
+            maxWidth: '600px',
+            margin: '5px',
+            textAlign: 'left',
+            backgroundColor: 'white'
+        }} sm={5} xl={12}>
+            <Col>
+                <h2>Waste-o-Meter</h2>
+                <h1 className={'big'}>
+                    ${ total }
+                </h1>
+                <Button variant={'warning'} onClick={click}>
+                    ShowDetail
+                </Button>
+                <List
+                    allPrograms={allPrograms}
+                    setShowSpecifics={setShowSpecifics}
+                    showSpecifics={showSpecifics}/>
+            </Col>
         </Container>
     )
 }
